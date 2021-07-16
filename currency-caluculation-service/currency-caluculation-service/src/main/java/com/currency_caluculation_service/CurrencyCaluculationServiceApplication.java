52ed22f78a7660f0f526099cb7b68a524f0ac3b4
Saidugari Prashanth <sprashanth@nisum.com>
2021-07-16 14:27:04 +0530

    16-07-2021

--- currency-caluculation-service/currency-caluculation-service/src/main/java/com/currency_caluculation_service/CurrencyCaluculationServiceApplication.java
+++ currency-caluculation-service/currency-caluculation-service/src/main/java/com/currency_caluculation_service/CurrencyCaluculationServiceApplication.java
@@ -4,14 +4,17 @@ package com.currency_caluculation_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.web.client.RestTemplate;
 
-@FeignClient("com.currency_caluculation_service")
+@EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyCaluculationServiceApplication {
 
 	public static void main(String[] args) {
--- limitservice1/limitservice1/src/main/java/com/limitservice1/Limitservice1Application.java
+++ limitservice1/limitservice1/src/main/java/com/limitservice1/Limitservice1Application.java
@@ -2,7 +2,9 @@ package com.limitservice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Limitservice1Application {
 
